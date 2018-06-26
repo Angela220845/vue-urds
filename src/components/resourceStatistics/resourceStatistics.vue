3fc526208cbfc5ba31599404f52e5877e4860727
taoyu <15801924724@163.com>
2018-06-26 13:23:42 +0800

    资源统计页面完成--增加icon、样式调整

--- src/components/resourceStatistics/resourceStatistics.vue
+++ src/components/resourceStatistics/resourceStatistics.vue
@@ -2,28 +2,34 @@
   <div>
     <div class="wraper">
       <div class="server">
-        <div class="resource">主机</div>
-        <div class="resource">CPU
-          <el-progress :percentage="80" color="red"></el-progress>
+        <div class="resource server_color icon iconfont icon-zhuji">主机
+          <div>5</div>
         </div>
-        <div class="resource">磁盘
-          <el-progress :percentage="20" color="blue"></el-progress>
+        <div class="resource cpu_color icon iconfont icon-cpu">CPU
+          <el-progress :percentage="80" color="#de1818"></el-progress>
+          <span>3/10</span>
         </div>
-        <div class="resource">内存
-          <el-progress :percentage="30" color="#8e71c7"></el-progress>
+        <div class="resource ram_color icon iconfont icon-neicun">内存
+          <el-progress :percentage="30" color="#0606a9"></el-progress>
+          <span>768/9334</span>
+        </div>
+        <div class="resource disk_color icon iconfont icon-cipan">磁盘
+          <el-progress :percentage="20" color="#8e4eab"></el-progress>
+          <span>30/1110</span>
         </div>
       </div>
       <div class="service">
-        <div class="resource">MySQL
+        <div class="resource icon iconfont icon-mysql">MySQL
+          <div>3</div>
         </div>
-        <div class="resource">MongoDB
-
+        <div class="resource icon iconfont icon-mongodb">MongoDB
+          <div>1</div>
         </div>
       </div>
       <div class="consumption">
         <div class="resource">暂无消费数据，请稍后</div>
       </div>
-  </div>
+    </div>
   </div>
 </template>
 <script>
@@ -40,7 +46,7 @@
     }
   }
 </script>
-<style lang="css">
+<style lang="css" scoped>
   .wraper > div > div {
     border-radius: 5px;
   }
@@ -51,7 +57,42 @@
     float: left;
     padding: 15px 10px;
     box-sizing: border-box;
+  }
+  .resource:before{
+    color: #ccc;
+    float: right;
+    font-size: 18px;
+  }
 
+  .el-progress {
+    margin: 10px 0px 0px 0px;
+  }
+
+  .resource > span {
+    color: #666;
+    font-size: 12px;
+
+  }
+
+  .server_color {
+    color: green;
+  }
+
+  .server_color > div {
+    margin-top: 25px;
+    font-size: 18px;
+  }
+
+  .cpu_color {
+    color: #de1818;
+  }
+
+  .disk_color {
+    color: #8e4eab;
+  }
+
+  .ram_color {
+    color: #0606a9;
   }
 
   .server .resource {
@@ -63,6 +104,12 @@
     width: 49%;
     margin-right: 16px;
     margin-top: 20px;
+    color: #128bb5;
+  }
+
+  .service .resource > div {
+    margin-top: 20px;
+    font-size: 18px;
   }
 
   .consumption .resource {
--- src/main.js
+++ src/main.js
@@ -7,13 +7,15 @@ import ElementUI from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css';
 import axios from 'axios';
 import '../static/css/reset.css'
+import './assets/fonts/iconfont.css';
+
 Vue.prototype.$http = axios
 
 Vue.config.productionTip = false
     // ***element ui 开始
 Vue.use(ElementUI);
 
-// 
+//
 router.beforeEach((to, from, next) => {
         if (to.meta.title) {
             document.title = to.meta.title
