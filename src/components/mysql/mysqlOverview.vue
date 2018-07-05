796cde3d2b5377229dcb59c4bf9df28a02347342
taoyu <15801924724@163.com>
2018-07-05 17:49:14 +0800

    新增mysql页面概览tab栏组件

--- src/components/mysql/mysqlOverview.vue
+++ src/components/mysql/mysqlOverview.vue
@@ -1,15 +1,32 @@
 <template>
     <div>
-    
-        概览概览概览概览概览
-    
+
+      <el-tabs type="border-card">
+        <el-tab-pane>
+          <span slot="label"><i class="el-icon-date"></i> 概览</span>
+          概览列表</br>
+          概览列表</br>
+
+          概览列表</br>
+          概览列表</br>
+
+          概览列表</br>
+
+        </el-tab-pane>
+        <el-tab-pane label="访问方式">访问方式列表</el-tab-pane>
+        <el-tab-pane label="运行状态">运行状态列表</el-tab-pane>
+        <el-tab-pane label="规格">规格列表</el-tab-pane>
+        <el-tab-pane label="架构：高可用">高可用图示</el-tab-pane>
+
+      </el-tabs>
+
     </div>
 </template>
 
 <script>
     export default {
-    
-    
-    
+
+
+
     }
 </script>
