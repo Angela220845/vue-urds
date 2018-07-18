42a3471dc67638f233515da4c0f5663b39091140
taoyu <15801924724@163.com>
2018-07-19 00:09:41 +0800

    将日期选择器提取为单独的组件，监控页面完成资源消耗折线图。

---
+++ src/components/mysql/components/datepicker.vue
@@ -0,0 +1,40 @@
+<template>
+    <el-date-picker v-model="startTime" type="datetime" placeholder="选择起始时间" align="right" :picker-options="startPicker">
+    </el-date-picker>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                queryTime: "",
+                startPicker: {
+                    shortcuts: [{
+                            text: "今天",
+                            onClick(picker) {
+                                picker.$emit("pick", new Date());
+                            }
+                        },
+                        {
+                            text: "昨天",
+                            onClick(picker) {
+                                const date = new Date();
+                                date.setTime(date.getTime() - 3600 * 1000 * 24);
+                                picker.$emit("pick", date);
+                            }
+                        },
+                        {
+                            text: "一周前",
+                            onClick(picker) {
+                                const date = new Date();
+                                date.setTime(date.getTime() - 3600 * 1000 * 24 * 7);
+                                picker.$emit("pick", date);
+                            }
+                        }
+                    ]
+                },
+                startTime: "",
+            }
+        }
+    }
+</script>
--- src/components/mysql/monitor.vue
+++ src/components/mysql/monitor.vue
@@ -1,113 +1,66 @@
 <template>
   <div>
-    <el-select v-model="service" placeholder="服务">
-      <el-option v-for="item in serviceTypeList" :key="item.value" :label="item.label" :value="item.value">
-      </el-option>
-    </el-select>
-    <el-select v-model="monitorType" placeholder="监控类型">
-      <el-option v-for="item in monitorTypeList" :key="item.value" :label="item.label" :value="item.value">
-      </el-option>
-    </el-select>
-    <el-date-picker v-model="value2" type="datetime" placeholder="选择日期时间" align="right" :picker-options="pickerOptions1">
-    </el-date-picker>
-    <div id="myChart" :style="{width: '100%', height: '400px'}"></div>
-
+    <div class="title">
+      <el-select v-model="serviceType" placeholder="服务">
+        <el-option v-for="item in serviceTypeList" :key="item.value" :label="item.label" :value="item.value">
+        </el-option>
+      </el-select>
+      <el-select v-model="monitorType" placeholder="监控类型">
+        <el-option v-for="item in monitorTypeList" :key="item.value" :label="item.label" :value="item.value">
+        </el-option>
+      </el-select>
+      <Datepicker></Datepicker>
+      <div id="resourceChart" :style="{width: '1200px', height: '400px'}"></div>
+    </div>
   </div>
 </template>
 
 <script>
+  import {
+    monitor
+  } from './model/monitor.js';
+  import Datepicker from './components/datepicker.vue'
   export default {
-
+    components: {
+      Datepicker
+    },
+    created() {
+      this.monitorData = monitor
+      this.serviceTypeList = monitor[0].selectList[0].serviceTypeList
+      this.monitorTypeList = monitor[0].selectList[0].monitorTypeList
+      this.echartsData = monitor[0].echartsOption
+      console.log(monitor)
+    },
+    mounted() {
+      this.drawResourceChart();
+    },
     data() {
       return {
-        serviceTypeList: [{
-          value: '选项1',
-          label: '主'
-        }, {
-          value: '选项2',
-          label: '从'
-        }],
-        service: '',
-        monitorTypeList: [{
-            value: '选项1',
-            label: '资源消耗'
-          }, {
-            value: '选项2',
-            label: 'MySQL数据库请求状态'
-          },
-          {
-            value: '选项3',
-            label: 'MySQL数据库连接状态'
-          },
-          {
-            value: '选项4',
-            label: 'MySQL数据库数据状态'
-          }
-        ],
-        monitorType: '',
-        queryTime: '',
-        pickerOptions1: {
-          shortcuts: [{
-            text: '今天',
-            onClick(picker) {
-              picker.$emit('pick', new Date());
-            }
-          }, {
-            text: '昨天',
-            onClick(picker) {
-              const date = new Date();
-              date.setTime(date.getTime() - 3600 * 1000 * 24);
-              picker.$emit('pick', date);
-            }
-          }, {
-            text: '一周前',
-            onClick(picker) {
-              const date = new Date();
-              date.setTime(date.getTime() - 3600 * 1000 * 24 * 7);
-              picker.$emit('pick', date);
-            }
-          }]
-        },
-        value1: '',
-        value2: '',
-        value3: '',
-        time: [],
-        resource: [],
-        option :{
-          xAxis: {
-            type: 'category',
-            data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
-          },
-          yAxis: {
-            type: 'value'
-          },
-          series: [{
-            data: [820, 932, 901, 934, 1290, 1330, 200],
-            type: 'line'
-          }]
-        }
-      }
+        monitorData: [],
+        serviceType: '服务类型值',
+        monitorType: '监控类型值',
+        monitorTypeList: [],
+        monitorTypeList: [],
+        resourceChartsData: {}
+      };
     },
     methods: {
-      getData() {
-        let myChart = this.$echarts.init(document.getElementById('myChart'))
-        myChart.setOption({
-          title: {
-            text: '资源消耗',
-            left: 'center'
-          },
-          tooltip: {},
-          xAxis: {
-            data: this.option.xVipData
-          },
-          yAxis: {},
-          series: [{
-            name: '每月会员增长曲线折线图',
-            type: 'line',
-            data: this.option.yVipData
-          }]
-        })
+      drawResourceChart() {
+        let resourceChart = this.$echarts.init(document.getElementById('resourceChart'))
+        resourceChart.setOption(this.echartsData);
       }
     }
   }
 </script>
+
+<style lang="css" scoped>
+  .title {
+    padding: 10px;
+    background-color: white;
+  }
+  
+  #myChart {
+    border: 1px dashed gray;
+    margin-top: 20px;
+  }
+</style>
