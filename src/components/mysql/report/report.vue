5b8e648753615a9e8b54cd5eff2108591bc694b0
taoyu <15801924724@163.com>
2018-07-26 00:20:32 +0800

    增加巡检报告组件及报告详情页

---
+++ src/components/mysql/report/report.vue
@@ -0,0 +1,56 @@
+<template>
+    <div>
+        <div class="title">
+            <span>MYSQL服务：</span>
+            <el-select v-model="mysqlServiceId" filterable placeholder="请选择可用区" :filter-method="filterMethod()">
+                <el-option v-for="item in mysqlIdList" :key="item.value" :label="item.label" :value="item.value">
+                </el-option>
+            </el-select>
+            <el-table :data="reportList" border style="width: 100%">
+                <el-table-column prop="Timestamp" label="巡检报告触发时间" width="180">
+                </el-table-column>
+                <el-table-column prop="ServerId" label="主机名" width="180">
+                </el-table-column>
+                <el-table-column prop="MysqlId" label="MySQL实例ID">
+                </el-table-column>
+                <el-table-column label="巡检报告状态">
+                    <template slot-scope="scope">
+                        <router-link :to="'/mysql_list/mysql_report/diagnosis_report?key='+scope.row.Timestamp+'&service_id='+scope.row.ServerId"><span>点击查看</span></router-link>
+                    </template>
+                    </el-table-column>
+                </el-table>
+        </div>
+    </div>
+</template>
+  
+
+<script>
+    import {
+        reportList,
+        mysqlIdList
+    } from '../model/report_model.js'
+    export default {
+        created() {
+            this.reportList = reportList
+            this.mysqlIdList = mysqlIdList
+        },
+        data() {
+            return {
+                reportList: [],
+                mysqlIdList: [],
+                mysqlServiceId: ''
+            }
+        },
+        methods: {
+            filterMethod() {
+                console.log(this.mysqlServiceId)
+            }
+        }
+    }
+</script>
+
+<style lang="css" scoped>
+    a {
+        color: #409EFF
+    }
+</style>
