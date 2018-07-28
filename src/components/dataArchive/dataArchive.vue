2600525e367c26b89669c3c5e9603caf05a7dfdb
taoyu <15801924724@163.com>
2018-07-28 18:39:46 +0800

    增加数据归档页面

--- src/components/dataArchive/dataArchive.vue
+++ src/components/dataArchive/dataArchive.vue
@@ -1,5 +1,65 @@
 <template>
     <div>
-        数据归档
+        <div class="title">
+            <el-table :data="reportList" border style="width: 100%">
+                <el-table-column prop="Timestamp" label="源数据库服务 ">
+                </el-table-column>
+                <el-table-column prop="ServerId" label="目标数据库服务 ">
+                </el-table-column>
+                <el-table-column prop="MysqlId" label="源库表 ">
+                </el-table-column>
+                <el-table-column prop="" label="目标库表 ">
+                </el-table-column>
+                <el-table-column prop="" label="源字符集  ">
+                </el-table-column>
+                <el-table-column prop="" label="目标字符集  ">
+                </el-table-column>
+                <el-table-column prop="" label="归档条件  ">
+                </el-table-column>
+                <el-table-column prop="" label="归档时间  ">
+                </el-table-column>
+                <el-table-column prop="" label="归档状态  ">
+                </el-table-column>
+                <el-table-column prop="" label="用户 ">
+                </el-table-column>
+                <el-table-column label="操作">
+                    <template slot-scope="scope">
+                                <span class="operate">归档数据</span>
+</template>
+                </el-table-column>
+
+                    
+            </el-table>
+        </div>
     </div>
 </template>
+  
+
+<script>
+    import {
+        reportList,
+    } from './model.js'
+    export default {
+        created() {
+            this.reportList = reportList
+        },
+        data() {
+            return {
+                reportList: [],
+                mysqlServiceId: ''
+            }
+        },
+        methods: {
+            filterMethod() {
+                console.log(this.mysqlServiceId)
+            }
+        }
+    }
+</script>
+
+<style lang="css" scoped>
+    .operate{
+        color: #409EFF;
+        cursor: pointer;
+    }
+</style>
